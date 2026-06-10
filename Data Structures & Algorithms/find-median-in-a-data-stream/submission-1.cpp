c1b084cbf57ebbb82114689ef680fedec8f32172
Anish Pal <ap08anish@gmail.com>
2026-06-10 14:39:33 +0530

    Add: find-median-in-a-data-stream - submission-1

---
+++ Data Structures & Algorithms/find-median-in-a-data-stream/submission-1.cpp
@@ -0,0 +1,33 @@
+class MedianFinder {
+public:
+    priority_queue<int , vector<int> , greater<int>>minHeap;
+    priority_queue<int>maxHeap;
+    MedianFinder() {
+        
+    }
+    
+    void addNum(int num) {
+        if(maxHeap.empty() || num < maxHeap.top()){
+            maxHeap.push(num);
+        }
+        else{
+            minHeap.push(num);
+        }
+
+        if(abs((int)maxHeap.size() - (int)minHeap.size()) > 1){
+            minHeap.push(maxHeap.top());
+            maxHeap.pop();
+        }
+        else if(maxHeap.size() < minHeap.size()){
+            maxHeap.push(minHeap.top());
+            minHeap.pop();
+        }
+    }
+    
+    double findMedian() {
+        if(maxHeap.size() > minHeap.size()){
+            return maxHeap.top();
+        }
+        return (maxHeap.top() + minHeap.top())/2.0;
+    }
+};
